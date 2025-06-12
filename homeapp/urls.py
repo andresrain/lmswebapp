fb69994cf44dca663ac7b15a1ea716a3c7b9d17c
andresrain <andres.rainrobles@gmail.com>
2025-06-11 21:12:57 -0400

    creando el home

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='homeapp_index'), # Ruta para el hommeapp
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from .models import AboutPage, ContactPage, Student, Notice, Teacher
 
 # Create your views here.
-def index(request):
-    return render(request, 'home/index.html') # este es el template que se va a renderizar
+#Miguel:
+def home(request):
+
+    return render(request, 'home.html')
+
+def about(request):
+    
+    return render(request, 'about.html')
+
+def contact(request):
+    contact_text = contact.objects.all()
+    data = {"contactDetails": contact_text}
+    return render(request, 'contact.html', data)
+
+
+
+
+
+
+
